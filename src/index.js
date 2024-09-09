86ddebf632f2d23162a95ba4adb5de9d8a085370
Podili Biswajit <biswajitofficial2002@gmail.com>
2024-09-09 18:48:43 +0530

    Utils added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import 'dotenv/config';
+import "dotenv/config";
 import ConnectDB from "./db/index.js";
+import app from "./app.js";
 
-ConnectDB();
+ConnectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("Express failed to start", error);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection failed", error);
+  });
