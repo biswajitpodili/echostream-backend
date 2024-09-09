3af1a0e279dcef3376daf9f38aa306c1002e2d2f
Podili Biswajit <biswajitofficial2002@gmail.com>
2024-09-10 01:19:43 +0530

    Setup Completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import ConnectDB from "./db/index.js";
-import app from "./app.js";
+import { app } from "./app.js";
 
 ConnectDB()
   .then(() => {
@@ -8,7 +8,7 @@ ConnectDB()
       console.error("Express failed to start", error);
     });
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
+      console.log(`⚙ Server is running on port ${process.env.PORT}`);
     });
   })
   .catch((error) => {
